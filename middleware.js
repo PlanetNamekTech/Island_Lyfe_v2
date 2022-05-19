987ead417b2ba6394cf63f5f741a26ee63ce02e0
Spencer Richardson <sr23513@gmail.com>
2022-05-18 18:35:50 -0700

    Added navbar and logout functionality

--- middleware.js
+++ middleware.js
@@ -1,5 +1,6 @@
 module.exports.isLoggedIn = (req,res,next) => {
   if(!req.isAuthenticated()) {                  // isAuthenticated() is part of passport, not found in their docs
+    req.session.returnTo = req.originalUrl;
     req.flash('error', 'You must be signed in')
     return res.redirect('/login');
   }
--- routes/users.js
+++ routes/users.js
@@ -13,8 +13,11 @@ router.post('/register', catchAsync(async (req,res) => {
     const {email, username, password} = req.body;
     const user = new User({ email, username });
     const registeredUser = await User.register(user, password);
-    req.flash('success', 'Welcome to the Island Lyfe');
-    res.redirect('/islands'); 
+    req.login(registeredUser, err => {
+      if(err) return next(err);
+      req.flash('success', 'Welcome to the Island Lyfe');
+      res.redirect('/islands'); 
+    })
   } catch(e) {
     req.flash('error', e.message);
     res.redirect('register')
@@ -26,8 +29,17 @@ router.get('/login', (req,res) => {
 })
 
 router.post('/login', passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), (req, res) => {
-  req.flash('success', 'Welcome Back');
-  res.redirect('/islands');
+  let { username } = req.body;
+  req.flash('success', `Welcome Back ${username}`);
+  const redirectUrl = req.session.returnTo || '/islands';
+  delete req.session.returnTo; // delete returnTo object from session, no need to store it
+  res.redirect(redirectUrl);
+})
+
+router.get('/logout', (req, res) => {
+  req.logout(); //Another built in passport function
+  req.flash('success', 'Successfully logged out');
+  res.redirect('/islands')
 })
 
 module.exports = router;
